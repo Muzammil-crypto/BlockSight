39324cc0ae5db6f8b74822b0ac454ef1e0d0143c
Apple <apple@Apples-MacBook-Pro.local>
2023-01-12 11:57:08 +0500

    theme responsiveness

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactPlayer from "react-player";
-
+import styles from "./styles.module.scss";
 export const VideoCard = () => {
   const URL =
     "https://www.youtube.com/watch?v=5jpgMXt1Z9Y&ab_channel=edureka%21";
   return (
-    <div>
-      <div style={{ marginTop: 40 }}>
-        <ReactPlayer url={URL} />
-      </div>
-    </div>
+    <div className={styles.videoCard}>{/* <ReactPlayer url={URL} /> */}</div>
   );
 };

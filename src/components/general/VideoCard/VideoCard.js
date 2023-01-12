bd54cd40978600995c1a943b50a5f9737dcc76d3
Apple <apple@Apples-MacBook-Pro.local>
2023-01-12 18:11:37 +0500

    Mobile responsiveness done

--- src/components/general/VideoCard/VideoCard.js
+++ src/components/general/VideoCard/VideoCard.js
@@ -5,8 +5,6 @@ export const VideoCard = () => {
   const URL =
     "https://www.youtube.com/watch?v=5jpgMXt1Z9Y&ab_channel=edureka%21";
   return (
-    <div className={styles.videoCard}>
-      <ReactPlayer url={URL} />
-    </div>
+    <div className={styles.videoCard}>{/* <ReactPlayer url={URL} /> */}</div>
   );
 };
